f974932b7d6845e732002f4e678f0d4fa3179cce
Emil <emilbabazade29@gmail.com>
2024-05-08 20:26:53 +0300

    refactoring tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,65 +1,50 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn echo_fails_no_args() {
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
+fn echo_fails_with_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo_rust")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn echo_runs_with_args() {
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
+fn echo_runs_with_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echo_rust")?;
     cmd.arg("poop").assert().success();
     cmd.arg("poop").arg("peep").assert().success();
+    Ok(())
 }
 
-#[test]
-fn echo_outputs_args_seperated_by_newline() {
-    let expected = "hello world\n";
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
-    cmd.arg("hello world").assert().success().stdout(expected);
-}
-
-#[test]
-fn echo_newline_single_arg() {
-    let expected = "hello world\n";
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
-    cmd.arg("hello world").assert().success().stdout(expected);
-}
-
-#[test]
-fn echo_newline_multiple_args() {
-    let expected = "hello\nworld\n";
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
-    cmd.arg("hello")
-        .arg("world")
+fn run(args: &[&str], expected_result: &str) -> TestResult {
+    Command::cargo_bin("echo_rust")?
+        .args(args)
         .assert()
         .success()
-        .stdout(expected);
+        .stdout(String::from(expected_result));
+    Ok(())
 }
 
 #[test]
-fn echo_no_newline_single_arg() {
-    let expected = "hello world ";
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
-    cmd.arg("-n")
-        .arg("hello world")
-        .assert()
-        .success()
-        .stdout(expected);
+fn echo_newline_single_arg() -> TestResult {
+    run(&["hello world"], "hello world\n")
 }
 
 #[test]
-fn echo_no_newline_multiple_args() {
-    let expected = "hello world ";
-    let mut cmd = Command::cargo_bin("echo_rust").unwrap();
-    cmd.arg("-n")
-        .arg("hello")
-        .arg("world")
-        .assert()
-        .success()
-        .stdout(expected);
+fn echo_newline_multiple_args() -> TestResult {
+    run(&["hello", "world"], "hello\nworld\n")
+}
+
+#[test]
+fn echo_no_newline_single_arg() -> TestResult {
+    run(&["hello world", "-n"], "hello world ")
+}
+
+#[test]
+fn echo_no_newline_multiple_args() -> TestResult {
+    run(&["-n", "hello", "world"], "hello world ")
 }
